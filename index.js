422ea52608d1cbb2a357abd88a7a9a1f788b5601
Gachure <your.email@example.com>
2024-01-09 10:01:31 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    // Target the paragraph element with id="text" and update its text content
+    var textParagraph = document.getElementById("text");
+    textParagraph.textContent = "This is really cool!";
+  });
+  
+  console.log("This console.log() fires when index.js loads - before DOMContentLoaded is triggered");
+  
